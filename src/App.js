b90cdd960e70258a891259a3849d6555a76534e7
Poonam Gahatraj <poonamgtrz@gmail.com>
2024-03-07 14:44:52 +0545

    project_amazon

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Amazon from './pages/amazon';
+
+
+ export default function App(){
+  return(
+    <Amazon/>
+  )
+ }
---
+++ src/pages/amazon.js
@@ -0,0 +1,14 @@
+export default function Amazon (){
+    return(
+        <>
+        <nav style={{height:"60px",backgroundColor:"#0F1111",color:"white"}}>
+            <div>
+                <div className="nav_logo">
+                    <img src="./amazon_logo (1).png"  style={{height:"50px",width:"100px"}}></img>
+                </div>
+            </div>
+
+        </nav>
+        </>
+    )
+}
